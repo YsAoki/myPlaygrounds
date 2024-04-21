5b78ac073515be5a4f5cc32c729bf8cf0543822c
YsAoki <kazuaki.aoki@ysinc.co.jp>
2024-04-21 09:30:04 +0900

    ページ遷移構造の作成。共通部分のヘッダーの作成

---
+++ src/components/organisms/Header/style.ts
@@ -0,0 +1,8 @@
+import { Typography, styled } from "@mui/material";
+
+
+export const SHeaderTitle = styled(Typography)`
+  font-weight: 500;
+  font-size: 24px;
+  flex-grow: 1;
+`
---
+++ src/pages/Top/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import TopLayout from "../../components/templates/TopLayout";
+
+const Top: FC = () => {
+  return <TopLayout />;
+};
+
+export default Top;

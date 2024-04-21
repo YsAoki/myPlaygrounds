9819e1918b07b72465a66f240507acb8b05639a9
YsAoki <kazuaki.aoki@ysinc.co.jp>
2024-04-21 09:20:19 +0900

    初回環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import "reset-css";
 import ReactRouter from "./pages/ReactRouter";
+import "./style.css";
 
 const App: FC = () => {
   return <ReactRouter />;
